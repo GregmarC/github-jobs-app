e6f11f0af9a56891dad0dced2858242ceab4c8e2
Greg Cruz D.O <49351487+x1jinchu1x@users.noreply.github.com>
2020-08-12 23:41:49 -0400

    post-deploy fix

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, Col, FormLabel } from 'react-bootstrap';
+import { Form, Col } from 'react-bootstrap';
 import './App.css';
 
 export default function SearchForm({ params, onParamChange }) {
